1ead2a60ac346b37dbf4c22c6a951d2253265c8b
jafrex <jafrex01@gmail.com>
2024-01-03 20:40:52 +0500

    TS Imports && Minor Html Structure Change

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 "use strict";
-console.log("Test");
+const question = document.getElementById("question");
+const optionOne = document.getElementById("option-1");
+const optionTwo = document.getElementById("option-2");
+const optionThree = document.getElementById("option-3");
+const optionFour = document.getElementById("option-4");
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log("Test");
+const question = document.getElementById("question") as HTMLHeadingElement | null;
+const optionOne = document.getElementById("option-1") as HTMLButtonElement | null;
+const optionTwo = document.getElementById("option-2") as HTMLButtonElement | null;
+const optionThree = document.getElementById("option-3") as HTMLButtonElement | null;
+const optionFour = document.getElementById("option-4") as HTMLButtonElement | null;
