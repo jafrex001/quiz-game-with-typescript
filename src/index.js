9dec0a4e1555b89d37349fb208d0ad42316c5c2e
jafrex <jafrex01@gmail.com>
2024-01-03 23:01:29 +0500

    -Added Display function for Questions and Options

--- src/index.js
+++ src/index.js
@@ -4,4 +4,52 @@ const optionOne = document.getElementById("option-1");
 const optionTwo = document.getElementById("option-2");
 const optionThree = document.getElementById("option-3");
 const optionFour = document.getElementById("option-4");
-question.innerText = "this is a test";
+const questionOne = "Test question 1";
+const questionTwo = "Test question 2";
+const questionThree = "Test question 3";
+const questionFour = "Test question 4";
+const questionFive = "Test question 5";
+optionOne.addEventListener("click", () => { });
+optionTwo.addEventListener("click", () => { });
+optionThree.addEventListener("click", () => { });
+optionFour.addEventListener("click", () => { });
+function displayQuestionOne() {
+    let options = ["Test Option 1", "Test Option 2", "Test Option 3", "Test Option 4"];
+    question.innerText = questionOne;
+    optionOne.innerText = options[0];
+    optionTwo.innerText = options[1];
+    optionThree.innerText = options[2];
+    optionFour.innerText = options[3];
+}
+function displayQuestionTwo() {
+    let options = ["Test Option 1", "Test Option 2", "Test Option 3", "Test Option 4"];
+    question.innerText = questionOne;
+    optionOne.innerText = options[0];
+    optionTwo.innerText = options[1];
+    optionThree.innerText = options[2];
+    optionFour.innerText = options[3];
+}
+function displayQuestionThree() {
+    let options = ["Test Option 1", "Test Option 2", "Test Option 3", "Test Option 4"];
+    question.innerText = questionOne;
+    optionOne.innerText = options[0];
+    optionTwo.innerText = options[1];
+    optionThree.innerText = options[2];
+    optionFour.innerText = options[3];
+}
+function displayQuestionFour() {
+    let options = ["Test Option 1", "Test Option 2", "Test Option 3", "Test Option 4"];
+    question.innerText = questionOne;
+    optionOne.innerText = options[0];
+    optionTwo.innerText = options[1];
+    optionThree.innerText = options[2];
+    optionFour.innerText = options[3];
+}
+function displayQuestionFive() {
+    let options = ["Test Option 1", "Test Option 2", "Test Option 3", "Test Option 4"];
+    question.innerText = questionOne;
+    optionOne.innerText = options[0];
+    optionTwo.innerText = options[1];
+    optionThree.innerText = options[2];
+    optionFour.innerText = options[3];
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,61 @@
 const question = document.getElementById("question") as HTMLHeadingElement;
-const optionOne = document.getElementById("option-1") as HTMLButtonElement | null;
-const optionTwo = document.getElementById("option-2") as HTMLButtonElement | null;
-const optionThree = document.getElementById("option-3") as HTMLButtonElement | null;
-const optionFour = document.getElementById("option-4") as HTMLButtonElement | null;
+const optionOne = document.getElementById("option-1") as HTMLButtonElement;
+const optionTwo = document.getElementById("option-2") as HTMLButtonElement;
+const optionThree = document.getElementById("option-3") as HTMLButtonElement;
+const optionFour = document.getElementById("option-4") as HTMLButtonElement;
 
-question.innerText = "this is a test";
+const questionOne: string = "Test question 1";
+const questionTwo: string = "Test question 2";
+const questionThree: string = "Test question 3";
+const questionFour: string = "Test question 4";
+const questionFive: string = "Test question 5";
+
+optionOne.addEventListener("click", () => {});
+optionTwo.addEventListener("click", () => {});
+optionThree.addEventListener("click", () => {});
+optionFour.addEventListener("click", () => {});
+
+function displayQuestionOne() {
+  let options: string[] = ["Test Option 1", "Test Option 2", "Test Option 3", "Test Option 4"];
+  question.innerText = questionOne;
+  optionOne.innerText = options[0];
+  optionTwo.innerText = options[1];
+  optionThree.innerText = options[2];
+  optionFour.innerText = options[3];
+}
+
+function displayQuestionTwo() {
+  let options: string[] = ["Test Option 1", "Test Option 2", "Test Option 3", "Test Option 4"];
+  question.innerText = questionOne;
+  optionOne.innerText = options[0];
+  optionTwo.innerText = options[1];
+  optionThree.innerText = options[2];
+  optionFour.innerText = options[3];
+}
+
+function displayQuestionThree() {
+  let options: string[] = ["Test Option 1", "Test Option 2", "Test Option 3", "Test Option 4"];
+  question.innerText = questionOne;
+  optionOne.innerText = options[0];
+  optionTwo.innerText = options[1];
+  optionThree.innerText = options[2];
+  optionFour.innerText = options[3];
+}
+
+function displayQuestionFour() {
+  let options: string[] = ["Test Option 1", "Test Option 2", "Test Option 3", "Test Option 4"];
+  question.innerText = questionOne;
+  optionOne.innerText = options[0];
+  optionTwo.innerText = options[1];
+  optionThree.innerText = options[2];
+  optionFour.innerText = options[3];
+}
+
+function displayQuestionFive() {
+  let options: string[] = ["Test Option 1", "Test Option 2", "Test Option 3", "Test Option 4"];
+  question.innerText = questionOne;
+  optionOne.innerText = options[0];
+  optionTwo.innerText = options[1];
+  optionThree.innerText = options[2];
+  optionFour.innerText = options[3];
+}
